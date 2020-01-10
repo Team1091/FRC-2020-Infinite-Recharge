0fae7b6f5513a1e41744658448c37f7477aefa97
jcdiem <jcdiem3@gmail.com>
2020-01-09 19:21:13 -0600

    Vision Subsystem base foundation - made empty functions for future implement - coordinate system to return target location - created the basic shell for the class

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.shared.TargetCoordinate;
+
+public class VisionSubsystem extends SubsystemBase {
+    private UsbCamera visionFeed = new UsbCamera("Camera1", 1);
+    private NetworkTableInstance networkTable  = null;
+    private NetworkTable gripNetworkTable  = null;
+
+
+    public VisionSubsystem(){
+        visionFeed.setResolution(640, 480);
+        CameraServer.getInstance().startAutomaticCapture(visionFeed);
+        networkTable = NetworkTableInstance.getDefault();
+        gripNetworkTable = NetworkTableInstance.getDefault().getTable("GRIP/myBlobsReport");
+
+    }
+
+    public boolean targetInSight() {
+        //todo implement
+        return false;
+    }
+
+    public double getDistanceToTarget () {
+        //todo implement
+        return 0.0;
+    }
+
+    public TargetCoordinate getTargetCoordinates() {
+        //todo implement
+        return null;
+    }
+
+}
