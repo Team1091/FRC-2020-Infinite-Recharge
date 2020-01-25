218fb0cfe9c952483ac4d34f0d46ad80fdf875b4
NateGnome <ohmnat@huhs.org>
2020-01-25 12:57:28 -0600

    Added the cool RunHopperMotorCommand its less cool than the other one

---
+++ src/main/java/frc/robot/commands/RunHopperMotorCommand.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.PickUpSubsystem;
+
+public class RunHopperMotorCommand extends CommandBase {
+    private HopperSubsystem hopperSubsystem;
+    private double speed;
+
+    public RunHopperMotorCommand(HopperSubsystem hopperSubsystem) {
+        this(hopperSubsystem, .5);
+    }
+
+    public RunHopperMotorCommand(HopperSubsystem hopperSubsystem, double speed) {
+        this.hopperSubsystem = hopperSubsystem;
+        this.speed = speed;
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        hopperSubsystem.runHopperMotor(speed);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        hopperSubsystem.runHopperMotor(0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
