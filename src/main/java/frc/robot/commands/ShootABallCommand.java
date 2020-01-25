41532b11b0845d401d358c545d867a7b2ad7412c
Andrew K <akneeland0330@gmail.com>
2020-01-25 09:19:49 -0600

    added some code to ShootABallCommand and added TakeAimCommand.

--- src/main/java/frc/robot/commands/ShootABallCommand.java
+++ src/main/java/frc/robot/commands/ShootABallCommand.java
@@ -1,25 +1,37 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShootABallCommand extends CommandBase {
     private ShooterSubsystem shooterSubsystem;
+    private boolean shot;
+    private Timer timer;
+    private double timeToShoot;
 
 
-    public ShootABallCommand(ShooterSubsystem shooterSubsystem) {
+    public ShootABallCommand(ShooterSubsystem shooterSubsystem, double timeToShoot) {
         super();
         this.shooterSubsystem = shooterSubsystem;
         addRequirements(shooterSubsystem);
+        timer = new Timer();
+        this.timeToShoot = timeToShoot;
     }
 
     @Override
     public void initialize() {
+        timer.reset();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        if(timer.hasPeriodPassed(timeToShoot)) {
+           shot = true;
+           return;
+        }
+        this.shooterSubsystem.shootshot();
     }
 
     // Called once the command ends or is interrupted.
@@ -30,7 +42,7 @@ public class ShootABallCommand extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return shot;
     }
 
 
---
+++ src/main/java/frc/robot/commands/TakeAimCommand.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class TakeAimCommand extends CommandBase {
+    public TakeAimCommand(){
+        super();
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+    }
+
+
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
